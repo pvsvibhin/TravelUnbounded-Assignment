5b20654224e36615709bc85c6147115abad9986a
pvsvibhin <pvsvibhin@gmail.com>
2023-08-03 10:35:32 +0000

    Travel Unbounded Assignment

---
+++ src/components/LadingPage/index.js
@@ -0,0 +1,85 @@
+import {Component} from 'react'
+import './index.css'
+
+class LandingPages extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <h1>Big Cats Week at Maasai Mara - Diwali 2023 special</h1>
+        <img
+          src="https://res.cloudinary.com/dedvz7flb/image/upload/v1690962390/Screenshot_588_rzw2nr.png"
+          alt="lion"
+        />
+        <p>
+          November is one of the Best times to visit Kenya, especially to visit
+          its prime park,This is because the large migratory herds would have
+          returned to Tanzania and now the Big cats have to venture long
+          distances to hunt their prey.November is also the season of short
+          rains. This makes the plains of Maasai Mara lush green. The skies are
+          truly dramatic and colourful.The Sun rise and Sun sets during November
+          at Mara is simply breath taking. November is also a time for
+          holidaying and getting together with our families. And the icing on
+          the cake is we have curated this itinerary for such an affordable
+          costs without compromising on any of the services. This is commemorate
+          our first year anniversary of opening up operations at Nairobi.Join us
+          on this unforgettable journey of your life time.
+        </p>
+        <div>
+          <h1>Itenary-</h1>
+          <p>
+            Day 1 - Nov 14th 2023 - Our drivers would pick up from the JKIA
+            airport at 7AM. You would board our 4*4 Landcruisers to embark on
+            your journey to Maasai Mara. The road journey takes about 5 to 6
+            hours, but you would be amazed by the landscape enroute. You would
+            first see the Nairobi national park on your left side. On a lucky
+            day, you would also see a White Rhino grazing near the fence.
+            Nairobi city has a national park right in its heart!! And it is home
+            for Lions, Leopards, White rhinos, gazzelles, Giraffes, crowned
+            cranes, and other mammals and birds. This is the only city in the
+            World which boasts a National park within the city limits.
+          </p>
+          <img
+            src="https://res.cloudinary.com/dedvz7flb/image/upload/v1690963715/Screenshot_589_vflfgj.png"
+            alt="day1"
+          />
+          <p>
+            After one hour you would climb up to The Great Rift Valley. This is
+            one priced attraction of Kenya.Formed a millions years back, this
+            valley boasts beautiful lakes and escarpments, which provide havens
+            for plentiful wildlife. The Great Rift Valley runs for 4,000 miles
+            (6,400 kilometers), from Jordan in southwestern Asia to the coast of
+            the Indian Ocean in central Mozambique. We suggest you request your
+            driver to stop when you are on top and enjoy the views of this
+            Nature’s marvel.You would reach Maasai Mara by noon. Complete your
+            check in formalities and proceed to lunch.After lunch, you would go
+            on your first game drive on the great plains of Maasai Mara!!
+          </p>
+          <img
+            src="https://res.cloudinary.com/dedvz7flb/image/upload/v1690964087/Screenshot_590_jqxla0.png"
+            alt="day-1-2"
+          />
+          <p>
+            This afternoon, our experienced Maasai guides/drivers would take you
+            around the Talek and Topi plains area. There you would find a lot of
+            ungulates, antelopes, zebras and also a huge pride of Lions. This
+            pride is called the ‘Topi Pride’. Relax, sit back and enjoy God’s
+            creations. You would be so excited to see the endless plains of
+            Maasai Mara with its dwellers.Return to your camp after 6PM. Refresh
+            yourself and head towards the camp fire. Meet up with your group and
+            listen to their first day experience at Mara. Share your magical
+            experiences with them. Have dinner and retire for the day ( LD )
+          </p>
+          <p>
+            Day 2 - Nov 15th - Wake up early. Freshen yourself and head to the
+            restaurant for a hot cup of coffee or tea. The mornings are cold at
+            Mara, so do wear your fleece jacket for the morning game drive.
+            Breakfast would be packed for you. Explore the mornings at Mara. The
+            Sunrise you witness at Mara is something which you would never get
+            to witness anywhere in the World. Just pray that it does not rain!!
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+export default LandingPages
